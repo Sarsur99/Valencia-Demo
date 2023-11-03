1cc6dad2364928ad9fc7d71fa1c9b5f8390a8b46
sarsur99 <sorenisme@gmail.com>
2023-11-03 15:17:32 -0700

    90%

--- src/App.js
+++ src/App.js
@@ -1,129 +1,190 @@
-  import React from 'react';
-  import { Button, Container, Typography, AppBar, Toolbar, IconButton } from '@mui/material';
-  import MenuIcon from '@mui/icons-material/Menu';
-  import ContactForm from './contact';
-  import ScrollAnimationObserver from './ScrollAnimationObserver';
+import React, { useState } from 'react';
+import { Button, Container, Typography, Paper, Grid } from '@mui/material';
+import { AppBar, Toolbar, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ContactForm from './contact';
+import ScrollAnimationObserver from './ScrollAnimationObserver';
+import './style.css';
+import valen from './images/valen.png'
+import factory from './images/factory.jpg'
+import sales from './images/Salesforce.com_logo.png'
+import zen from './images/Zendesk-Logo.png'
+import eco from './images/eco-tek.png'
+import Brightness4Icon from '@mui/icons-material/Brightness4'; // Icon for the lightbulb button
 
 
-  function App() {
-    return (
-      
-      <div>
+const logoStyle = {
+  height: '180px', // Set the height you desire
+  width: '250px',
+};
 
-        <AppBar position="static" style={{ backgroundColor: 'linear-gradient(45deg, #263238 30%, #FF8E53 90%)' }}>
-          <Container>
-            <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
-              {/* Logo */}
-              <Typography
-                variant="h4"
-                component="h1"
-                sx={{
-                  color: '#fff',
-                  flexGrow: 1,
-                  fontFamily: 'Montserrat, sans-serif', // Change to a modern sans-serif font
-                  fontSize: '36px', // Adjust the font size
-                  fontWeight: 'bold', // Make the text bold
-                  letterSpacing: '1px', // Add some letter-spacing
-                  textTransform: 'uppercase', // Optionally, make the text uppercase
-                }}
-              >
-                Valencia AI
-              </Typography>
-              {/* Navigation */}
-              <div>
-                <Button color="inherit">About</Button>
-                <Button color="inherit">Investment</Button>
-                {/* Use anchor tag to jump to the contact section */}
-                <Button href="#contact" color="inherit">
-                  Contact
-                </Button>
-              </div>
-              <IconButton color="inherit" edge="end">
-                <MenuIcon />
-              </IconButton>
-            </Toolbar>
-          </Container>
-        </AppBar>
-        
+const salesStyle = {
+  height: '150px',
+  width: '150px'
+}
 
-        {/* Hero Section */}
-        <ScrollAnimationObserver>
-        <section id="hero" className="hero animate-on-scroll" style={{ backgroundImage: 'url("")', backgroundSize: 'cover', backgroundPosition: 'center', padding: '100px 0', textAlign: 'center', color: '#fff' }}>
-          <Container maxWidth="sm">
+const appBarStyle = {
+  background: 'linear-gradient(45deg, #263238 30%, #FF8E53 90%)', // Gradient background
+  boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.2)', // Add box shadow for dimension
+};
+
+const appBarTextStyle = {
+  fontSize: '36px', // Increase font size
+  fontWeight: '600', // Make it bold
+  color: 'white', // White text color
+  textTransform: 'uppercase', // Uppercase text
+  letterSpacing: '2px', // Adjust letter-spacing
+  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', // Add a subtle text shadow
+  fontFamily: 'Your-Selected-Font', // Use a custom or Google Font
+};
+
+
+function App() {
+  // State variable to track dark mode status
+  
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Function to toggle dark mode
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  // Apply the 'dark-mode' class to the body element when dark mode is enabled
+  if (darkMode) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+  return (
+    <div>
+      <AppBar position="static" style={appBarStyle}>
+        <Container>
+          <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Typography variant="h3" sx={{ marginBottom: '0', color: 'white', fontFamily: 'nunito' }}>
+              Valencia AI
+            </Typography>
             <div>
-              <Typography variant="h2" sx={{ marginBottom: '20px' }}>
-                Valencia AI
-              </Typography>
-              <Typography variant="h5" sx={{ marginBottom: '40px' }}>
-                Your captivating introduction goes here.
-              </Typography>
-              <Button variant="contained" color="secondary">
-                Get Started
+              <Button href="#about" color="inherit">
+                About
               </Button>
+              <Button href="#testimony" color="inherit">
+                Testimony
+              </Button>
+              <Button href="#contact" color="inherit">
+                Contact
+              </Button>
+              <IconButton color="inherit" onClick={toggleDarkMode}>
+                <Brightness4Icon /> {/* Lightbulb icon */}
+              </IconButton>
             </div>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <section id="hero" className="hero animate-on-scroll" style={{
+        backgroundImage: `url(${factory})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        padding: '100px 0',
+        textAlign: 'center',
+        color: '#fff'
+      }}>
+        <Container maxWidth="sm">
+          <div>
+            <img src={valen} alt='logo' style={logoStyle} />
+            {/* <Typography variant="h2" sx={{ marginBottom: '18px', color: 'white' }}>
+              Valencia AI
+            </Typography> */}
+            {/* <Typography variant="h5" sx={{ marginBottom: '40px', color: 'white' }}>
+              Driving Innovation with AI: Revolutionizing Auto Manufacturing
+            </Typography> */}
+          </div>
+        </Container>
+      </section>
+
+      <ScrollAnimationObserver animationType="fade-in"
+        duration={1000} // Animation duration (in milliseconds)
+        className="about-section" // Add the CSS class you defined
+      >
+        <section id="about" className="aboutly" style={{ padding: '60px 0', textAlign: 'center' }}>
+          <Container maxWidth="lg">
+            <Paper elevation={3} sx={{ padding: '20px' }}>
+              <Typography variant="h2" sx={{ marginBottom: '18px' }}>
+                About Us
+              </Typography>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <Typography variant="body1" sx={{ textAlign: 'left' }}>
+                    Welcome to Valencia AI! We are passionate about driving innovation in the field of artificial intelligence and transforming the auto manufacturing industry. Founded in 2021 in San Diego County, Valencia AI strives to create cutting-edge solutions that revolutionize the way vehicles are designed, manufactured, and brought to market
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="body1" sx={{ textAlign: 'left' }}>
+                    Our dedicated team of experts, engineers, and researchers is committed to pushing the boundaries of innovation. Our mission is to bring about a sustainable and intelligent future for the automotive industry by harnessing the power of AI and technology. With never before seen methods of artificial vertical integration, Valencia AI is setting forth a new trajectory for in house program development for both manufacturers and distributors alike.
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button href="#contact" variant="contained" color="primary">
+                    Learn More
+                  </Button>
+                </Grid>
+              </Grid>
+            </Paper>
           </Container>
         </section>
-        </ScrollAnimationObserver>
+      </ScrollAnimationObserver>
 
-        {/* Features Sectio n */}
-        <ScrollAnimationObserver>
-        <section className="features" style={{ backgroundColor: '#f9f9f9', padding: '60px 0', textAlign: 'center' }}>
-          <Container maxWidth="md">
-            <Typography variant="h2" sx={{ marginBottom: '40px' }}>
-              Features
-            </Typography>
-            <div className="feature">
-              <img src="" alt="Feature 1" style={{ width: '200px', height: '200px', marginBottom: '20px' }} />
-              <Typography variant="h4" sx={{ marginBottom: '10px' }}>
-                Feature 1
-              </Typography>
-              <Typography variant="body1" sx={{ color: '#555', marginBottom: '20px' }}>
-                Description of feature 1 goes here.
-              </Typography>
-            </div>
-            {/* Add more feature items as needed */}
-          </Container>      
-        </section>
-        </ScrollAnimationObserver>
+      <section id="testimony" className="testimony" style={{ padding: '60px 0', textAlign: 'center' }}>
+        <Container maxWidth="lg">
+          <Typography variant="h2" sx={{ marginBottom: '18px' }}>
+            Testimonies
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={4}>
+              <Paper elevation={3} sx={{ padding: '20px' }}>
+                <Typography variant="h5" sx={{ textAlign: 'left' }}>
+                  "Outstanding resources that enable us to be at our best! Valencia is paving the way for new-age vertical integration for the automotive industry"
+                </Typography>
+                <img src={sales} alt='alt' style={salesStyle} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Paper elevation={3} sx={{ padding: '20px' }}>
+                <Typography variant="h5" sx={{ textAlign: 'left' }}>
+                  "Valencia AI has created some cutting edge tools for the automotive industry that are yet to show the full extent of their impact"
+                </Typography>
+                <img src={zen} alt='zen' style={salesStyle} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Paper elevation={3} sx={{ padding: '20px' }}>
+                <Typography variant="h5" sx={{ textAlign: 'left' }}>
+                  "Following the aquisiticion of Eco-Tek Group LLC; Valencia AI group LLC has confirmed a change of ETEK's business model, redirecting its focus to Next Generation AI and AI technologies" 
+                </Typography>
+                <img src={eco} alt="eco" style={salesStyle} />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </section>
 
-        {/* Testimonials Section */}
-        <section className="testimonials" style={{ backgroundColor: '#ebebeb', padding: '60px 0', textAlign: 'center' }}>
-          <Container maxWidth="md">
-            <Typography variant="h2" sx={{ marginBottom: '40px' }}>
-              Testimonials
-            </Typography>
-            <div className="testimonial">
-              <img src="path/to/testimonial-1-image.jpg" alt="Testimonial 1" style={{ width: '100px', height: '100px', borderRadius: '50%', marginBottom: '20px' }} />
-              <Typography variant="body1" sx={{ color: '#555', marginBottom: '20px' }}>
-                "Testimonial 1 text goes here."
-              </Typography>
-              <Typography variant="body2" sx={{ fontStyle: 'italic' }}>
-                - Testimonial Author
-              </Typography>
-            </div>
-            {/* Add more testimonial items as needed */}
-          </Container>
-        </section>
-
-        {/* Contact Section */}
-        <ScrollAnimationObserver>
-        <section  id="contact" className="contact animate-on-scroll" style={{ padding: '60px 0', textAlign: 'center' }}>
+      <ScrollAnimationObserver>
+        <section id="contact" className="contact animate-on-scroll" style={{ padding: '30px 0', textAlign: 'center' }}>
           <Container maxWidth="sm">
             <ContactForm />
           </Container>
         </section>
-        </ScrollAnimationObserver>
+      </ScrollAnimationObserver>
 
-        {/* Footer Section */}
-        <footer style={{ backgroundColor: '#444', color: '#fff', textAlign: 'center', padding: '20px 0' }}>
-          <Container>
-            <Typography variant="body2">
-              &copy; {new Date().getFullYear()} Valencia AI. All rights reserved.
-            </Typography>
-          </Container>
-        </footer>
-      </div>
-    );
-  }
+      <footer style={{ backgroundColor: '#444', color: '#fff', textAlign: 'center', padding: '20px 0' }}>
+        <Container>
+          <Typography variant="body2">
+            &copy; {new Date().getFullYear()} Valencia AI. All rights reserved.
+          </Typography>
+        </Container>
+      </footer>
+    </div>
+  );
+}
 
-  export default App;
+export default App;
